344e7221a687a61b4a60916017e1c302e0fe74b0
João Marcos <joaomarcos@frwk.com.br>
2024-03-04 12:23:35 -0300

    feat: add timer completed, readme

---
+++ src/FocusTimer/sounds.js
@@ -0,0 +1,11 @@
+export const buttonPressAudio = new Audio("./assets/button-press.wav");
+
+export const kitchenTimer = new Audio("./assets/kitchen-timer.mp3");
+
+export const bgAudio = new Audio("./assets/bg-audio.mp3");
+
+buttonPressAudio.volume = 0.5;
+kitchenTimer.volume = 0.5;
+
+bgAudio.loop = true;
+bgAudio.volume = 0.5;
